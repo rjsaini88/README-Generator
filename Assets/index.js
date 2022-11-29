9c1be4b5a85982ac62325146c7067122de668e7a
Rj Saini <rsaini88@gmail.com>
2022-11-28 17:26:12 -0800

    Initial Comit with provided code

---
+++ Assets/index.js
@@ -0,0 +1,20 @@
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+const questions = [
+{
+    type: "input",
+    message: "what is the title of your project?",
+    name: "title" 
+}
+
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
