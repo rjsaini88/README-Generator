e73d3c9b0928076002164fdd9e87be250717402a
Rj Saini <rsaini88@gmail.com>
2022-11-29 16:50:50 -0800

    Added code to index.js from activity and generateMarckdown.js

---
+++ Assets/Test.md
@@ -0,0 +1,2 @@
+# Testing Read Me
+
--- Assets/index.js
+++ Assets/index.js
@@ -1,20 +1,65 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown')
 // TODO: Create an array of questions for user input
 const questions = [
+    //Option if you want to give user the option to name thier README file. 
+    //Make sure to enable code in the init function.
 {
     type: "input",
-    message: "what is the title of your project?",
-    name: "title" 
+    name: "fileName" ,
+    message: "What do you want to name you README file?",
+},
+{
+    type: "input",
+    name: "title" ,
+    message: "What is the title of your project?",
 }
 
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFileSync(fileName, data);
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then(response => { 
+        const md = generateMarkdown(response)
+        // writeToFile('README.md',md) //Hard coded for the file name to be README.md
+        writeToFile(`${response.fileName}.md`, md)
+    })  
+}
 
 // Function call to initialize app
 init();
+
+
+
+
+// .prompt([
+//     {
+//       type: 'input',
+//       name: 'name',
+//       message: 'What is your name?',
+//     },
+//     {
+//       type: 'checkbox',
+//       message: 'What languages do you know?',
+//       name: 'stack',
+//       choices: ['HTML', 'CSS', 'JavaScript', 'MySQL'],
+//     },
+//     {
+//       type: 'list',
+//       message: 'What is your preferred method of communication?',
+//       name: 'contact',
+//       choices: ['email', 'phone', 'telekinesis'],
+//     },
+// .then((response) =>
+//     response.confirm === response.password
+//       ? console.log('Success!')
+//       : console.log('You forgot your password already?!')
+//   );
